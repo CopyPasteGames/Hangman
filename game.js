ff66bf815a7dcbf5b5e99f92263f09039a7b55d0
kgsensei <jt@kellem.com>
2022-03-25 22:08:12 -0400

    Initial Commit

---
+++ game.js
@@ -0,0 +1,235 @@
+$(window).bind("load",()=>{
+	loadNewGame()
+	$('#copyPasteLogo').delay(200).fadeIn(250)
+	$('#copyPasteLogo').delay(975).fadeOut(250)
+	$("#introScreen").delay(1700).fadeOut(750)
+})
+
+compliments=[
+	'Awesome!',
+	'Nice Job!',
+	'Doin\' Great!',
+	'Amazing!',
+	'Well Done!',
+	'Wow!',
+	'You\'re Great!',
+	'Super!',
+	'Super Cool!',
+	'Very Cool!',
+	'Super Awesome!'
+]
+
+alphabet=[
+	'a',
+	'b',
+	'c',
+	'd',
+	'e',
+	'f',
+	'g',
+	'h',
+	'i',
+	'j',
+	'k',
+	'l',
+	'm',
+	'n',
+	'o',
+	'p',
+	'q',
+	'r',
+	's',
+	't',
+	'u',
+	'v',
+	'w',
+	'x',
+	'y',
+	'z'
+]
+
+wordList=[
+	'hello',
+	'world',
+	'engine',
+	'jazz',
+	'unreal',
+	'united',
+	'states',
+	'hosting',
+	'number',
+	'steam',
+	'information',
+	'character',
+	'fantastic',
+	'greatest',
+	'social',
+	'clock',
+	'timer',
+	'wireless',
+	'interface',
+	'internet',
+	'videos',
+	'chat',
+	'rainy',
+	'days',
+	'assets',
+	'pictures',
+	'documents',
+	'keeper',
+	'drive',
+	'truck',
+	'flunk',
+	'trunk',
+	'index',
+	'projects',
+	'connected',
+	'water',
+	'song',
+	'cheese',
+	'failure',
+	'beans',
+	'cringe',
+	'culture',
+	'creeper',
+	'mining',
+	'daughter',
+	'absolute',
+	'styles',
+	'number',
+	'indeed',
+	'future',
+	'opinion',
+	'expectations',
+	'rolling',
+	'increasing',
+	'community',
+	'actually',
+	'clash',
+	'royal',
+	'crash',
+	'cash',
+	'money',
+	'history',
+	'galaxy',
+	'through',
+	'common',
+	'english',
+	'words',
+	'together',
+	'children',
+	'child',
+	'toward',
+	'war',
+	'pattern',
+	'against',
+	'center',
+	'person',
+	'bank',
+	'govern',
+	'science',
+	'appear',
+	'server',
+	'mountain',
+	'north',
+	'began',
+	'young',
+	'usual',
+	'enough',
+	'measure',
+	'product',
+	'numeral',
+	'complete',
+	'question',
+	'happen',
+	'remember',
+	'hundred',
+	'thousand',
+	'between',
+	'city',
+	'cities',
+	'story',
+	'interest',
+	'language',
+	'among',
+	'weight',
+	'perhaps',
+	'brought',
+	'thousand',
+	'close',
+	'night',
+	'day',
+	'light',
+	'darkness',
+	'dark',
+	'spire',
+	'ten',
+	'western',
+	'eastern',
+	'southern',
+	'northern',
+	'step',
+	'true',
+	'king',
+	'queen',
+	'prince',
+	'heard',
+	'farm',
+	'pass',
+	'early'
+]
+
+wordToGuess='None'
+failedGuesses=[]
+passedGuesses=[]
+livesLeft=6
+
+function renderWord(){
+	var string=''
+	for(let i=0;i<wordToGuess.length;i++){
+		if(passedGuesses.indexOf(wordToGuess[i])!=-1){
+			string=string+wordToGuess[i].toUpperCase()+" "
+		}else{
+			string=string+"_ "
+		}
+	}
+	$('#wordToGuess').html(string)
+	if(string.removeSpace().toLowerCase()==wordToGuess.toLowerCase()){
+		newGameMessage(kg.randomItem(compliments))
+	}
+}
+
+function makeGuess(elem,letter){
+	if(wordToGuess.indexOf(letter.toLowerCase())!=-1){
+		passedGuesses.push(letter.toLowerCase())
+		$(elem).css({"background-color":"rgba(40,240,40,0.5)"})
+		renderWord()
+	}else{
+		if(failedGuesses.indexOf(letter.toLowerCase())==-1){
+			livesLeft=livesLeft-1
+			if(livesLeft==0)newGameMessage("Word Failed.")
+			$('#hangmanImg').attr({"src":"./assets/hangman"+(6-livesLeft)+".png"})
+			$(elem).css({"background-color":"rgba(255,40,40,0.5)"})
+			failedGuesses.push(letter.toLowerCase())
+		}
+	}
+}
+
+function newGameMessage(message){
+	$('#endingMessage').html(message)
+	$('#overlay').fadeIn(500)
+}
+
+function loadNewGame(){
+	$('#guessButtons').html('')
+	$('#hangmanImg').attr({"src":"./assets/hangman0.png"})
+	wordToGuess=kg.randomItem(wordList)
+	failedGuesses=[]
+	passedGuesses=[]
+	livesLeft=6
+	for(let i=0;i<alphabet.length;i++){
+		var letter=alphabet[i].toUpperCase()
+		$('#guessButtons').append(`<div class="guessBtn" onclick="makeGuess(this,'${letter}')">${letter}</div>`)
+	}
+	renderWord()
+}
